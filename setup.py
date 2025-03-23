d57ff1102470eb408b9d3e6e4263792eed1e6ec9
Shoyaib01 <sabbirahmmedshoyaib@gmail.com>
2025-03-23 17:10:51 +0600

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'AI Medical Chatbot',
+    version= '0.0.0',
+    author= 'S.M. Sabbir Ahmed',
+    author_email= 'sabbirahmedshoyaib@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
